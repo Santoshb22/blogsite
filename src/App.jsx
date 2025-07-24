6240948934bc2c3e74eaa0d10c61f8a88a2d67f1
Santoshb22 <sb289848@gmail.com>
2025-07-24 15:19:50 +0530

    added redux store for auth and manage user exist or not

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import { useEffect } from 'react';
-import { account } from './lib/appwrite';
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/appwrite';
+import { login, logout } from './store/authSlice';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router';
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+
   useEffect(() => {
-    const checkAppwrite = async () => {
-      try {
-        const user = await account.get();
-        console.log("User info:", user);
-      } catch (error) {
-        console.error("Appwrite connection error:", error.message);
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}));
+      } else {
+        dispatch(logout());
       }
-    };
-    checkAppwrite();
-  }, []);
+    })
+    .finally(() => setLoading(false))
+  }, [])
 
-  return <h1>Hello from Appwrite Test</h1>;
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-[#0f172a] text-white'>
+      <div className='w-full block bg-[#1e293b]'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
-export default App;
+export default App
